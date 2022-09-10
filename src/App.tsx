5bad5b151a613fa4e590a2c9d801647d15727109
GBDM97 <ghian.m@gmail.com>
2022-09-10 18:51:07 -0300

    buton testing a basic promise function

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css';
+import React from 'react';
 import nasa from './Media/nasa1.jpeg'
+import { resolve } from 'node:path/win32';
 
 function App() {
+
+const [sts, set] = React.useState('Not Loaded');
+
+const load = () => {
+  set('Loading...')
+  new Promise(resolve => setTimeout(resolve, 2000))
+  .then(()=>set('Loaded'))
+}
+
   return (
-    <>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    <img src={nasa} alt=""/>
-    </>
+      <button onClick={load} style={{backgroundColor: 'lime'}}>{sts}</button>
   );
 }
 
