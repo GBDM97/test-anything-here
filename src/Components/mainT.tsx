d2f0a7b25aa94c1aca4d9402228109d3297acb1d
GBDM97 <ghian.m@gmail.com>
2022-09-13 00:03:08 -0300

    testing lightweight version of portfolio title

---
+++ src/Components/mainT.tsx
@@ -0,0 +1,37 @@
+import './mainT.css'
+
+const MT = () => {
+    return(
+<>
+<div style={{width: '100vw', height: '30vmin', backgroundColor: 'black', justifyContent: 'center', display: 'flex'}}>
+    
+    <div className='aT15' style={{animationDelay: '0s'}} 
+    >HELLO!</div>
+    
+    <div className='aT15' style={{animationDelay: '2s'}} 
+    >I AM A DEVELOPER</div>
+    
+    <div className='aT15' style={{animationDelay: '4s'}} 
+    >THAT'S MOTIVATED</div>
+
+    <div className='aT15' style={{animationDelay: '4s', backgroundColor: 'transparent'}} 
+    ><br/> TO HAVE NEW IDEAS </div>
+    
+    <div className='aT15' style={{animationDelay: '6.5s'}} 
+    >AND ACCOMPLISH</div>
+
+    <div className='aT15' style={{animationDelay: '6.5s', backgroundColor: 'transparent'}} 
+    ><br/>THE HARDEST GOALS</div>
+    
+    <div className='aT15' style={{animationDelay: '9s'}} 
+    >MY NAME IS</div>
+    
+    <div className='aT15' style={{animationName: 'typewriterTitle2',animationDelay: '11s'}} 
+    >GHIANCARLO MORELLI</div>
+
+
+
+</div>
+</>)};
+
+export default MT
