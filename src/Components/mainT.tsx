8670cd9515d109338411b4889fb9530e5fd424eb
GBDM97 <ghian.m@gmail.com>
2022-09-13 02:51:30 -0300

    testing entire website render

--- src/Components/mainT.tsx
+++ src/Components/mainT.tsx
@@ -3,34 +3,24 @@ import './mainT.css'
 const MT = () => {
     return(
 <>
-<div style={{width: '100vw', height: '30vmin', backgroundColor: 'black', justifyContent: 'center', display: 'flex'}}>
-    
+<div style={{width: '100vw', backgroundColor: 'black', justifyContent: 'center', 
+alignItems: 'center', display: 'flex'}}>
     <div className='aT15' style={{animationDelay: '0s'}} 
     >HELLO!</div>
-    
-    <div className='aT15' style={{animationDelay: '2s'}} 
+    <div className='aT15' style={{animationDelay: '0.5s'}} 
     >I AM A DEVELOPER</div>
-    
-    <div className='aT15' style={{animationDelay: '4s'}} 
+    <div className='aT15m' style={{animationDelay: '3.5s'}} 
     >THAT'S MOTIVATED</div>
-
-    <div className='aT15' style={{animationDelay: '4s', backgroundColor: 'transparent'}} 
+    <div className='aT15m' style={{animationDelay: '3.5s', backgroundColor: 'transparent'}} 
     ><br/> TO HAVE NEW IDEAS </div>
-    
-    <div className='aT15' style={{animationDelay: '6.5s'}} 
+    <div className='aT15m' style={{animationDelay: '5s'}} 
     >AND ACCOMPLISH</div>
-
-    <div className='aT15' style={{animationDelay: '6.5s', backgroundColor: 'transparent'}} 
+    <div className='aT15m' style={{animationDelay: '5s', backgroundColor: 'transparent'}} 
     ><br/>THE HARDEST GOALS</div>
-    
-    <div className='aT15' style={{animationDelay: '9s'}} 
+    <div className='aT15' style={{animationDelay: '7.5s'}} 
     >MY NAME IS</div>
-    
-    <div className='aT15' style={{animationName: 'typewriterTitle2',animationDelay: '11s'}} 
+    <div className='aT15' style={{animationName: 'typewriterTitle2', animationDelay: '9.5s'}} 
     >GHIANCARLO MORELLI</div>
-
-
-
 </div>
 </>)};
 
